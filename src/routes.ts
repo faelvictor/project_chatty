94965e5ad3ce49897acdf74de668da53ce1e4f76
Rafael Victor <faelvictor@users.noreply.github.com>
2021-04-29 13:13:38 -0300

    Init data base
    Configuration typeorm framework,configuration database SQLite and created routes

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { SettingsControllers } from "./controllers/SettingsControllers";
+
+const routes = Router();
+
+const settingsControllers = new SettingsControllers();
+
+routes.post("/settings", settingsControllers.create)
+
+export { routes };
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import express from "express";
+import "./database";
+import { routes } from "./routes"
 
 const app = express();
 
-app.get("/", (resquest, response) => {
-    return response.json({
-        message: "Olá mundo!!!"
-    })
-});
+app.use(express.json());
 
-app.post("/", (request, response) =>{
-    return response.json({ message: "Usuário salvo com sucesso!"});
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
